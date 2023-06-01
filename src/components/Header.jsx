986b549a9ca6bcd4f656dde586d6ce67519238d7
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2023-06-01 07:22:45 -0500

    b

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import trollFace from 'src/assets/troll-face.png' 
+
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="\troll-face.png" 
+                src={trollFace} 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
