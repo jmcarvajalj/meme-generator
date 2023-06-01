a38940fa3e8ec85571d03c7e610e6b2f425012b4
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2023-06-01 07:07:23 -0500

    move asset source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="src\assets\troll-face.png" 
+                src="\troll-face.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
